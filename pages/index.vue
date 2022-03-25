5596537cd6d563f4f6a48bce93348a3a4107e9b7
Alex Largo <alexlar163@gmail.com>
2022-03-25 14:38:55 -0500

    Delete all unnecessary components

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,4 @@
-<template>
-  <div>
-    <header>Este es un header</header>
-    <!-- nav -->
-    <nav>
-      <ul>
-        <li>
-          <router-link to="/">Home</router-link>
-        </li>
-        <li>
-          <router-link to="/about">About</router-link>
-        </li>
-        <li>
-          <router-link to="/contact">Contact</router-link>
-        </li>
-      </ul>
-    </nav>
-    <button>Botton</button>
-    <main>
-      <router-view></router-view>
-    </main>
-    <footer>
-      <p>Este es un footer</p>
-    </footer>
-  </div>
-</template>
+<template></template>
 
 <script>
 export default {
