8e1c2493cc192c5e9ce3795959a894d6ce8a1e38
Alex Largo <alexlar163@gmail.com>
2022-03-24 21:21:58 -0500

    :tada: Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>Este es un header</header>
+    <button>Botton</button>
+    <button>Botton</button>
+    <button>Botton</button>
+    <Tutorial />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
