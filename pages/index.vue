b277b329cbaaba29b3a9a4d6a45963325a9195d3
Alex Largo <alexlar163@gmail.com>
2022-03-24 21:39:14 -0500

    Add navigation

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <header>Este es un header</header>
+    <!-- nav -->
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/about">About</router-link>
+        </li>
+        <li>
+          <router-link to="/contact">Contact</router-link>
+        </li>
+      </ul>
+    </nav>
     <button>Botton</button>
     <Tutorial />
   </div>
